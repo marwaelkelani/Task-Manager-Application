f67374a2330ccff5bf2ae00757f9d4a6e23c8553
marwaelkelani <mar.elkelani@gmail.com>
2023-08-30 12:36:53 -0400

    updated routing to add task/edit task

--- taskmobile/src/app/tabs/tabs-routing.module.ts
+++ taskmobile/src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
+       {
+        path: 'edit-task/:task_id',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
       {
         path: '',
         redirectTo: '/tabs/tab1',
